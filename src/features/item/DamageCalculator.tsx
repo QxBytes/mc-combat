3c569e0796e76c732c8dd71f8c466c9cec9bcc43
QxBytes <qxbytes@gmail.com>
2021-06-26 00:21:59 -0700

    Commit before removing tests

--- src/features/item/DamageCalculator.tsx
+++ src/features/item/DamageCalculator.tsx
@@ -1,6 +1,6 @@
 import { Col, Row } from "react-bootstrap";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
-import { selectDamage, selectEntity } from "./activeSlice";
+import { selectDamage, selectEntity, setDamageType } from "./activeSlice";
 import { getSetEPF, setType } from "./armor";
 import { Entity } from "./entity";
 import { takeDamage } from "./maths";
@@ -34,7 +34,7 @@ interface DamageCalculatorType {
 export function DamageCalculator(props : DamageCalculatorType) {
     const entity : Entity = useAppSelector(selectEntity);
     const damage : Damage = useAppSelector(selectDamage);
-    const dispatch = useAppDispatch();
+    
     return (
         <div className="container-top">
             <Row noGutters className="">
@@ -42,7 +42,7 @@ export function DamageCalculator(props : DamageCalculatorType) {
             <h3 className="text-left bottom-border p-1">Damage Calculations</h3>
             </Col>
             </Row>
-            <Row className="damage-display-container container-bottom">
+            <Row className="damage-display-container">
                 <DamageDisplay dmg={{amount: damage.amount, type: d.MELEE}}></DamageDisplay>
                 <DamageDisplay dmg={{amount: damage.amount, type: d.FALL}}></DamageDisplay>
                 <DamageDisplay dmg={{amount: damage.amount, type: d.EXPLOSION}}></DamageDisplay>
@@ -61,8 +61,15 @@ interface DamageDisplayType {
 }
 export function DamageDisplay(props: DamageDisplayType) {
     const entity : Entity = useAppSelector(selectEntity);
+    const damage : Damage = useAppSelector(selectDamage);
+    const dispatch = useAppDispatch();
+    const [hover, setHover] = useState(false);
     return (
-        <Col>
+        <Col onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)}
+        className={hover || props.dmg.type === damage.type ? "highlighted" : ""}
+        onClick={ () => dispatch(setDamageType(props.dmg.type))}
+        >
+                
             <h4>{props.dmg.type}</h4>
             <h6>Resistance {round(100- ((takeDamage(props.dmg, entity))*100 / props.dmg.amount))}%</h6>
             <h6>{round(takeDamage(props.dmg, entity))}</h6>
--- src/features/item/activeSlice.ts
+++ src/features/item/activeSlice.ts
@@ -57,7 +57,7 @@ export const activeSlice = createSlice( {
     }
 });
 
-export const {setType, setEnchant, removeEnchant} = activeSlice.actions;
+export const {setType, setEnchant, removeEnchant, setDamageType, setDamage} = activeSlice.actions;
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
